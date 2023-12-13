56b672ef3032691ee45e9d8a28b54690d2670b2d
JaishreeNishad <Jaysh2222@gmail.com>
2023-12-13 13:35:26 +0530

    display() function Displays the value of clicled button

--- script.js
+++ script.js
@@ -1,4 +1,18 @@
-// This function displays the values
+
 function display(value) {
-    document.getElementById("result").value += value;
+    document.getElementById("result").value += value;// display the value//
+}
+
+
+ function clearValue(){
+     document.getElementById("result").value = "";//Clear the display screen//
+
+}
+
+// / This function evaluates the expression and returns the result
+function calculate() {
+    var p = document.getElementById("result").value;
+   
+    var q = eval(p);
+    document.getElementById("result").value = q;
 }
